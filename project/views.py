257e812d739ffc3a26c38651c815120a948598cb
lisorius <148572778+lisorius@users.noreply.github.com>
2024-03-02 13:53:18 +0300

    Update views.py
    ...

--- project/views.py
+++ project/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
 def test(request):
     return render(request,'test.html')
-
+@csrf_exempt
 def index(request):
-        return render(request, 'index.html')
+
+        HTML = render(request, 'index.html')
+        if request.method=='POST':
+            HTML.delete_cookie('NAME')
+            HTML.delete_cookie('NON')
+            return HTML
+        HTML.set_cookie('NON', 'R{OIGJ{OIGJO{GOJGGOGHJDOUSPHGUSDGUUJSHGUHDGPGHNP{DNSG{PIJGN')
+
+        HTML.set_cookie('NAME', 'Yroslav')
+        return HTML
